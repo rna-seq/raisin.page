9eb00330a0464cfef8760a62de97e65b9aafc4d0
Maik R��der <maikroeder@gmail.com>
2011-07-09 12:15:44 +0000

    =add docstring

--- raisin/page/__init__.py
+++ raisin/page/__init__.py
@@ -1,3 +1,14 @@
+"""Provide information about pages
+
+PAGES: Read from the pages.ini file
+
+    * Used by raisin.pyramid to register pages in Pyramid
+    
+    * Used by raisin.restyler to render pages
+    
+"""
+
 import os
 from configobj import ConfigObj
+
 PAGES = ConfigObj(os.path.join(os.path.dirname(__file__), "pages.ini"))
